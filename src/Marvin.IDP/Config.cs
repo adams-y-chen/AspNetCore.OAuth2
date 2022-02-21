8e0bc82dccd90eff7ac96dbf93a982bedfccbfaa
adams-y-chen <yuhechen2108@gmail.com>
2022-02-20 20:54:48 -0500

    register ImageGallery MVC web application to IdenityServer.

--- src/Marvin.IDP/Config.cs
+++ src/Marvin.IDP/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -24,6 +25,28 @@ namespace Marvin.IDP
 
         public static IEnumerable<Client> Clients =>
             new Client[] 
-            { };
+            {
+                // Our MVC web app in project ImageGallery.Client
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient", // client application ID which client presents when request authroization code
+                    AllowedGrantTypes = GrantTypes.Code, // Allow Authorization Code flow
+                    RedirectUris = new List<string>() // Redirect user to Image Gallery MVC app after user authenticates to IdentityServer.
+                    {
+                        "https://localhost:44389/signin-oidc" // URL of the Image Gallery MVC application
+                    },
+                    AllowedScopes =
+                    {
+                        // Specify scopes that client application can request access to.
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256()) // client secret which client presents
+                    }
+                }
+            };
     }
 }
